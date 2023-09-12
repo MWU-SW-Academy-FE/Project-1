4533c2b740f7b28e11dad2f1068b8674d13c8711
mk1751041 <https://github.com/mk1751041>
2023-09-12 14:24:42 +0900

    계산기 기능 관련 DOM 셋팅

--- script.js
+++ script.js
@@ -4,7 +4,28 @@ class Calculator{
 
 const cal = new Calculator()
 
+// 값 표시
 const $previousPreview = document.querySelector('[data-previous-preview]')
 const $currentPreview = document.querySelector('[data-current-preview]')
-console.log($previousPreview)
-console.log($currentPreview)
+
+// 사칙연산
+const $plus = document.querySelector('[data-btn-plus]')
+const $minus = document.querySelector('[data-btn-minus]')
+const $multiple = document.querySelector('[data-btn-multiple]')
+const $divide = document.querySelector('[data-btn-divide]')
+
+// 숫자, 연산
+const $numbers = document.querySelectorAll('[data-btn-number]')
+const $operations = document.querySelectorAll('[data-btn-operation]')
+
+$numbers.forEach(($number) => {
+    $number.addEventListener('click', (e) => {
+        console.log(e.target.textContent)
+    })
+})
+
+$operations.forEach(($operation) => {
+    $operation.addEventListener('click', (e) => {
+        console.log(e.target.textContent)
+    })
+})
